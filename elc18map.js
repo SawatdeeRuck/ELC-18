918dcc29fa7927d728b37ff72d7aaf54921d3d4f
SawatdeeRuck <ruckh@yahoo.com>
2017-08-01 17:15:21 -0400

    zoom level, lat lng center

--- elc18map.js
+++ elc18map.js
@@ -6,10 +6,10 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 17,
+        zoom: 14,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(26.3415378,-80.0809695), // Boca Resort -- Boca Raton Florida
+        center: new google.maps.LatLng(26.3411533,-80.0909687), // Boca Resort -- Boca Raton Florida
 
         // How you would like to style the map. 
         // This is where you would paste any style found on Snazzy Maps.
@@ -120,7 +120,7 @@ function init() {
 
     // Let's also add a marker while we're at it
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(26.3415378,-80.0809695),
+        position: new google.maps.LatLng(26.3411533,-80.0909687),
         map: map,
         title: 'Boca Resort'
     });
